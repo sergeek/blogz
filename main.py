b5f96b16eda0cabce18f050fc691f6ee183e77ec
Sergey Polishchuk <sergeek@live.com>
2017-07-15 16:59:16 -0700

    modified main.py for postgreSQL

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz:password@localhost:8889/blogz')
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'MakeAWish'
